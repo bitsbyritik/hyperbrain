c757f54b367371a717746839ab96ccb8f604cbdb
bitsbyritik <bitsbyritik@gmail.com>
2025-04-29 23:50:23 +0530

    landing-page updated

--- apps/web/components/home/navbar.tsx
+++ apps/web/components/home/navbar.tsx
@@ -15,111 +15,114 @@ import {
 import { NAV_LINKS } from "@/utils";
 import React from "react";
 import { FaArrowCircleRight } from "react-icons/fa";
+import AnimationContainer from "../global/animate-container";
 
 const Navbar = () => {
   return (
-    <header
-      className={cn(
-        "px-4 h-14 sticky top-0 inset-x-0 w-full bg-card backdrop-blur-md border rounded-2xl border-border z-30",
-      )}
-    >
-      <div className={cn("flex items-center justify-between h-full mx-auto")}>
-        <div className="flex gap-6 items-center">
-          <Link href="/" className="flex items-center gap-2 cursor-pointer">
-            <BrainIcon />
-            <span className="text-xl font-bold">HyperBrain</span>
-          </Link>
+    <AnimationContainer reverse delay={0.1}>
+      <header
+        className={cn(
+          "px-4 h-14 sticky top-0 inset-x-0 w-full bg-card backdrop-blur-md border rounded-2xl border-border z-30",
+        )}
+      >
+        <div className={cn("flex items-center justify-between h-full mx-auto")}>
+          <div className="flex gap-6 items-center">
+            <Link href="/" className="flex items-center gap-2 cursor-pointer">
+              <BrainIcon />
+              <span className="text-xl font-bold">HyperBrain</span>
+            </Link>
 
-          <NavigationMenu className="hidden lg:flex">
-            <NavigationMenuList>
-              {NAV_LINKS.map((link) => (
-                <NavigationMenuItem key={link.title}>
-                  {link.menu ? (
-                    <>
-                      <NavigationMenuTrigger>
-                        {link.title}
-                      </NavigationMenuTrigger>
-                      <NavigationMenuContent>
-                        <ul
-                          className={cn(
-                            "grid gap-1 p-4 md:w-[400px] lg:w-[500px] rounded-xl",
-                            link.title === "Features"
-                              ? "lg:grid-cols-[.75fr_1fr]"
-                              : "lg:grid-cols-2",
-                          )}
-                        >
-                          {link.title === "Features" && (
-                            <li className="row-span-4 pr-2 relative rounded-lg overflow-hidden">
-                              <div className="absolute inset-0 !z-10 h-full w-[calc(100%-10px)] bg-[linear-gradient(to_right,rgb(38,38,38,0.5)_1px,transparent_1px),linear-gradient(to_bottom,rgb(38,38,38,0.5)_1px,transparent_1px)] bg-[size:1rem_1rem]"></div>
-                              <NavigationMenuLink
-                                asChild
-                                className="z-20 relative"
-                              >
-                                <Link
-                                  href="/"
-                                  className="flex h-full w-full select-none flex-col justify-end rounded-lg bg-gradient-to-b from-muted/50 to-muted p-4 no-underline outline-none focus:shadow-md"
+            <NavigationMenu className="hidden lg:flex">
+              <NavigationMenuList>
+                {NAV_LINKS.map((link) => (
+                  <NavigationMenuItem key={link.title}>
+                    {link.menu ? (
+                      <>
+                        <NavigationMenuTrigger>
+                          {link.title}
+                        </NavigationMenuTrigger>
+                        <NavigationMenuContent>
+                          <ul
+                            className={cn(
+                              "grid gap-1 p-4 md:w-[400px] lg:w-[500px] rounded-xl",
+                              link.title === "Features"
+                                ? "lg:grid-cols-[.75fr_1fr]"
+                                : "lg:grid-cols-2",
+                            )}
+                          >
+                            {link.title === "Features" && (
+                              <li className="row-span-4 pr-2 relative rounded-lg overflow-hidden">
+                                <div className="absolute inset-0 !z-10 h-full w-[calc(100%-10px)] bg-[linear-gradient(to_right,rgb(38,38,38,0.5)_1px,transparent_1px),linear-gradient(to_bottom,rgb(38,38,38,0.5)_1px,transparent_1px)] bg-[size:1rem_1rem]"></div>
+                                <NavigationMenuLink
+                                  asChild
+                                  className="z-20 relative"
                                 >
-                                  <h6 className="mb-2 mt-4 text-lg font-medium">
-                                    All Features
-                                  </h6>
-                                  <p className="text-sm leading-tight text-muted-foreground">
-                                    Manage links, track performance, and more.
-                                  </p>
-                                </Link>
-                              </NavigationMenuLink>
-                            </li>
-                          )}
-                          {link.menu.map((menuItem) => (
-                            <ListItem
-                              key={menuItem.title}
-                              title={menuItem.title}
-                              href={menuItem.href}
-                              icon={menuItem.icon}
-                            >
-                              {menuItem.tagline}
-                            </ListItem>
-                          ))}
-                        </ul>
-                      </NavigationMenuContent>
-                    </>
-                  ) : (
-                    <Link href={link.href} passHref>
-                      <NavigationMenuLink
-                        className={navigationMenuTriggerStyle()}
-                      >
-                        {link.title}
-                      </NavigationMenuLink>
-                    </Link>
-                  )}
-                </NavigationMenuItem>
-              ))}
-            </NavigationMenuList>
-          </NavigationMenu>
-        </div>
-        <div className="flex gap-4 items-center">
-          <div className="flex gap-1">
-            <a
-              href={"https://github.com/bitsbyritik/hyperbrain"}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="mt-1"
-            >
-              <FaDiscord size={34} />
-            </a>
-            <a
-              href={"https://github.com/bitsbyritik/hyperbrain"}
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <DiGithubBadge size={38} />
-            </a>
+                                  <Link
+                                    href="/"
+                                    className="flex h-full w-full select-none flex-col justify-end rounded-lg bg-gradient-to-b from-muted/50 to-muted p-4 no-underline outline-none focus:shadow-md"
+                                  >
+                                    <h6 className="mb-2 mt-4 text-lg font-medium">
+                                      All Features
+                                    </h6>
+                                    <p className="text-sm leading-tight text-muted-foreground">
+                                      Manage links, track performance, and more.
+                                    </p>
+                                  </Link>
+                                </NavigationMenuLink>
+                              </li>
+                            )}
+                            {link.menu.map((menuItem) => (
+                              <ListItem
+                                key={menuItem.title}
+                                title={menuItem.title}
+                                href={menuItem.href}
+                                icon={menuItem.icon}
+                              >
+                                {menuItem.tagline}
+                              </ListItem>
+                            ))}
+                          </ul>
+                        </NavigationMenuContent>
+                      </>
+                    ) : (
+                      <Link href={link.href} passHref>
+                        <NavigationMenuLink
+                          className={navigationMenuTriggerStyle()}
+                        >
+                          {link.title}
+                        </NavigationMenuLink>
+                      </Link>
+                    )}
+                  </NavigationMenuItem>
+                ))}
+              </NavigationMenuList>
+            </NavigationMenu>
           </div>
-          <Link href={"/home"}>
-            <FaArrowCircleRight size={34} className="fill-[#9158ed]" />
-          </Link>
+          <div className="flex gap-4 items-center">
+            <div className="flex gap-1">
+              <a
+                href={"https://github.com/bitsbyritik/hyperbrain"}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="mt-1"
+              >
+                <FaDiscord size={34} />
+              </a>
+              <a
+                href={"https://github.com/bitsbyritik/hyperbrain"}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <DiGithubBadge size={38} />
+              </a>
+            </div>
+            <Link href={"/home"}>
+              <FaArrowCircleRight size={34} className="fill-[#9158ed]" />
+            </Link>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </AnimationContainer>
   );
 };
 
--- apps/web/components/pages/landing-page.tsx
+++ apps/web/components/pages/landing-page.tsx
@@ -1,7 +1,49 @@
+import { ArrowRightIcon } from "lucide-react";
+import AnimationContainer from "../global/animate-container";
+import { Button } from "@workspace/ui/components/button";
+import Link from "next/link";
+
 export const LandingPage = () => {
-    return (
-        <div>
-            Landing Page
+  return (
+    <div className="overflow-x-hidden scrollbar-hide size-full">
+      <div className="mt-16 px-4 md:px-8 lg:px-12">
+        <div className="flex flex-col items-center justify-center w-full text-center bg-gradient-to-t from-background">
+          <AnimationContainer className="flex flex-col items-center justify-center w-full text-center">
+            <button className="group relative grid overflow-hidden rounded-full px-4 py-1 shadow-[0_1000px_0_0_hsl(0_0%_20%)_inset] transition-colors duration-200">
+              <span>
+                <span className="spark mask-gradient absolute inset-0 h-[100%] w-[100%] animate-flip overflow-hidden rounded-full [mask:linear-gradient(white,_transparent_50%)] before:absolute before:aspect-square before:w-[200%] before:rotate-[-90deg] before:animate-rotate before:bg-[conic-gradient(from_0deg,transparent_0_340deg,white_360deg)] before:content-[''] before:[inset:0_auto_auto_50%] before:[translate:-50%_-15%]" />
+              </span>
+              <span className="backdrop absolute inset-[1px] rounded-full bg-neutral-950 transition-colors duration-200 group-hover:bg-neutral-900" />
+              <span className="h-full w-full blur-md absolute bottom-0 inset-x-0 bg-gradient-to-tr from-primary/20"></span>
+              <span className="z-10 py-0.5 text-sm text-neutral-100 flex items-center justify-center gap-1">
+                ✨ Manage links smarter
+                <ArrowRightIcon className="ml-1 size-3 transition-transform duration-300 ease-in-out group-hover:translate-x-0.5" />
+              </span>
+            </button>
+            <h1 className="text-foreground text-center py-6 text-5xl font-medium tracking-normal text-balance sm:text-6xl md:text-7xl lg:text-8xl !leading-[1.15] w-full font-heading">
+              Smart Links, Smarter{" "}
+              <span className="text-transparent bg-gradient-to-r from-violet-500 to-fuchsia-500 bg-clip-text inline-bloc">
+                Thinking.
+              </span>
+            </h1>
+            <p className="mb-12 text-lg tracking-tight text-muted-foreground md:text-xl text-balance">
+              Centralize your links, notes, and AI in one brain with Hyperbrain.
+              <br className="hidden md:block" />
+              <span className="hidden md:block">
+                Turn your links into living knowledge.
+              </span>
+            </p>
+            <div className="flex items-center justify-center whitespace-nowrap gap-4 z-50">
+              <Button asChild>
+                <Link href={"/home"} className="flex items-center">
+                  Get Started
+                  <ArrowRightIcon className="w-4 h-4 ml-2" />
+                </Link>
+              </Button>
+            </div>
+          </AnimationContainer>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
